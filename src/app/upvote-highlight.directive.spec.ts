64e2f85c5196a0d91e10c75c4f29ea28928723f5
sylvestus <sylvestus.sigei@student.moringaschool.com>
2022-02-07 09:27:25 +0300

    add upvote highlight dirrective test file

---
+++ src/app/upvote-highlight.directive.spec.ts
@@ -0,0 +1,8 @@
+import { UpvoteHighlightDirective } from './upvote-highlight.directive';
+
+describe('UpvoteHighlightDirective', () => {
+  it('should create an instance', () => {
+    const directive = new UpvoteHighlightDirective();
+    expect(directive).toBeTruthy();
+  });
+});
